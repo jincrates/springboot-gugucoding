f38c65e0e9098210adbd13fc2bff5d193e4d9445
jincrates <94jingyu@gmail.com>
2021-08-12 00:13:26 +0900

    페이징처리 테스트 코드 작성

--- ex02/src/test/java/com/jincrates/ex02/repository/MemoRepositoryTest.java
+++ ex02/src/test/java/com/jincrates/ex02/repository/MemoRepositoryTest.java
@@ -4,6 +4,9 @@ import com.jincrates.ex02.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -59,5 +62,45 @@ public class MemoRepositoryTest {
 
         System.out.println("================================================================");
         System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate() {
+
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+
+        System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete() {
+
+        Long mno = 99L;
+
+        memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result =  memoRepository.findAll(pageable);
+
+        //데이터 확인
+        System.out.println(result);
+        System.out.println("=========================================================================");
+        System.out.println("Total Pages: "    + result.getTotalPages());    //총 몇 페이지
+        System.out.println("Total Count: "    + result.getTotalElements()); //전체 개수
+        System.out.println("Page Number: "    + result.getNumber());        //현재 페이지 번호
+        System.out.println("Page Size: "      + result.getSize());          //페이지당 데이터 개수
+        System.out.println("has next page?: " + result.hasNext());          //다음 페이지 존재여부
+        System.out.println("first page?: "    + result.isFirst());          //시작 페이지(0) 여부
+        System.out.println("=========================================================================");
+
+        //데이터 가져오기
+        for(Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
     }
 }
