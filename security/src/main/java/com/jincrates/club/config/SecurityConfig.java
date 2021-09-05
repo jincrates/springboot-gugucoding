e5fcef3481ab10d3d9b0a3657df64dd93b9950a4
jincrates <94jingyu@gmail.com>
2021-09-05 14:08:17 +0900

    Remember me와 @PreAuthorize

--- security/src/main/java/com/jincrates/club/config/SecurityConfig.java
+++ security/src/main/java/com/jincrates/club/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.jincrates.club.config;
 
 import com.jincrates.club.security.handler.ClubLoginSuccessHandler;
+import com.jincrates.club.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;  //주입
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -21,15 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // permitAll() : 모든 사용자에게 허락한다는 의미로 로그인을 하지 않은 사용자도 익명의 사용자로 간주되어 접근이 가능함
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+        //permitAll() : 모든 사용자에게 허락한다는 의미로 로그인을 하지 않은 사용자도 익명의 사용자로 간주되어 접근이 가능함
+        //http.authorizeRequests()
+        //        .antMatchers("/sample/all").permitAll()
+        //        .antMatchers("/sample/member").hasRole("USER");
 
         http.formLogin();  //권한 인증에 문제시 로그인 화면 이동
-        http.csrf().disable();  // csrf 토큰 비활성화
-
+        http.csrf().disable();  //csrf 토큰 비활성화
+        http.logout();
         http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService); //7dayss
     }
 
     @Bean
--- security/src/main/java/com/jincrates/club/controller/SampleController.java
+++ security/src/main/java/com/jincrates/club/controller/SampleController.java
@@ -1,6 +1,9 @@
 package com.jincrates.club.controller;
 
+import com.jincrates.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample/")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll...............");
@@ -20,8 +24,20 @@ public class SampleController {
         log.info("exMember......................");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin..................");
+    }
+
+    //로그인 된 사용자라 하더라도 user95가 아니면 에러 메시지 표기
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@jincrates.com\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
+
+        log.info("exMemberOnly.............");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 }
